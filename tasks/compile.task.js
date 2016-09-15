2e9fade5413c91e28646bd74f43701f2c0097de7
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2016-09-15 06:32:50 -0400

    Save lib output

--- tasks/compile.task.js
+++ tasks/compile.task.js
@@ -3,13 +3,16 @@ const sourcemaps = require('gulp-sourcemaps')
 const sass = require('gulp-sass')
 
 const config = require('./config')
+const outputConfig = {
+  outputStyle: 'expanded'
+}
 
 module.exports = function(BrowserSyncServer) {
   function CompileTask(done) {
     return gulp
       .src(config.paths.stylesheets)
       .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass(outputConfig).on('error', sass.logError))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest(config.paths.dist))
       .pipe(BrowserSyncServer.stream())
